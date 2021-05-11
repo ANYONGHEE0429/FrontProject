292eacba738442224d418ed2850fa516e04ecdd0
kimtaewon <kimtaewon@gmail.com>
2021-05-11 15:18:57 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const leftContent = document.querySelector('.left-div');
+const rightContent = document.querySelector('.right-div');
+const container = document.querySelector('.container');
+
+leftContent.addEventListener('click', e => {
+    container.style.opacity = '0';
+
+    setTimeout(() => {
+        document.location.href = './html/test.html';
+    }, 1000);
+});
+
+rightContent.addEventListener('click', e => {
+    container.style.opacity = '0';
+
+    setTimeout(() => {
+        document.location.href = './html/test.html';
+    }, 1000);
+});
+
+window.onpageshow = e => {
+
+    if (e.persisted) {
+        container.style.opacity = '1';
+    }
+
+}
