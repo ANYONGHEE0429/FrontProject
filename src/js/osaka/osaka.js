c96a5d047c0d6239b3958af5040eac925d8eeb32
Kimtaewon9483 <48502945+Kimtaewon9483@users.noreply.github.com>
2021-05-18 17:29:44 +0900

    2021-05-18 pm

--- src/js/osaka/osaka.js
+++ src/js/osaka/osaka.js
@@ -4,6 +4,12 @@ const title = document.querySelector('.first-warpper-title');
 const array = document.querySelectorAll('.section');
 const wrapper = document.querySelector('.wrapper');
 
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
+
+const imgBack = document.querySelector('.img-back');
+const imgCover = document.querySelector('.img-cover');
+
 let wheel = 0;
 let currentSection = 0;
 
@@ -18,10 +24,46 @@ new ScrollMagic
 let direction = '';
 let flag = false;
 
-window.addEventListener('wheel', e => {
+
+new ScrollMagic
+    .Scene({
+        triggerElement: array[1],
+        triggerHook: .5
+    })
+    .setClassToggle(imgBack, 'show')
+    .addTo(new ScrollMagic.Controller());
+
+new ScrollMagic
+    .Scene({
+        triggerElement: array[1],
+        triggerHook: .2
+    })
+    .setClassToggle(imgCover, 'show')
+    .addTo(new ScrollMagic.Controller());
+
+
+window.addEventListener('load', () => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].offsetTop == pageYOffset) {
+            currentSection = i;
+            pageYOffset = array[currentSection].offsetTop;
+        }
+    }
+});
+
+window.addEventListener('resize', () => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].offsetTop == pageYOffset) {
+            currentSection = i;
+            pageYOffset = array[currentSection].offsetTop;
+        }
+    }
+});
+
+wrapper.addEventListener('wheel', e => {
+
 
     e.preventDefault();
-
     // 스크롤을 아래로 내림
     if (0 < e.deltaY) {
         if ( currentSection >= array.length-1 ) { return }
@@ -59,8 +101,28 @@ function pageMove(direction) {
 
         setTimeout(() => {
             flag = false;
-        }, 1500);
+        }, 1300);
 
     }
  
 }
+
+// prev.addEventListener('click', () => {
+
+//     if ( currentSection <= 0 ) { return }
+//     else {
+//         currentSection--;
+//         scrollTo( {top: array[currentSection].offsetTop, behavior: 'smooth'} )
+//     }
+
+// });
+
+// next.addEventListener('click', () => {
+    
+//     if ( currentSection >= array.length-1 ) { return }
+//     else {
+//         currentSection++;
+//         scrollTo( {top: array[currentSection].offsetTop, behavior: 'smooth'} )
+//     }
+
+// })
