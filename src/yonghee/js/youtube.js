2198a7714bba8ba6cc78e80f406c264314c1ac0f
Kimtaewon9483 <48502945+Kimtaewon9483@users.noreply.github.com>
2021-05-19 09:41:44 +0900

    te

--- src/yonghee/js/youtube.js
+++ src/yonghee/js/youtube.js
@@ -9,14 +9,15 @@ function onYouTubePlayerAPIReady() {
   new YT.Player('player', {
     videoId: voidId, // 최초 재생할 유튜브 영상 ID
     playerVars: {
-      autoplay: true, // 자동 재생 유무
-      loop: true, // 반복 재생 유무
+      autoplay: 1, // 자동 재생 유무
+      loop: 1, // 반복 재생 유무
       playlist: voidId, // 반복 재생할 유튜브 영상 ID 목록
       controls: 0,
       disablekb: 1,
       rel: 0,
       showinfo: 0,
       enablejsapi: 1,
+      modestbranding: 1,
     },
     events: {
       // 영상이 준비되었을 때,
